d5ace4baf14a02a9ae51c4c619978deef52f0659
Procoderomega <gomballestas@gmail.com>
2026-02-15 12:54:34 -0600

    Refactor Antifly.Check for improved velocity checks
    - Added a new raycast system to detect the height of the player between the floor, if it passes the Maximun negative vertical speed it rubberbands the player via a system of flags

--- Logic module.lua
+++ Logic module.lua
@@ -1,13 +1,31 @@
+-- Version: 3.2
+
 local Antifly = {}
 -- logik
 function Antifly.Check(player, hrp, data, config)
+	
 	local velocity = hrp.AssemblyLinearVelocity
-	local vertical = math.abs(velocity.Y)
+	local vertical = (velocity.Y)
 	local horizontal = Vector3.new(velocity.X, 0 , velocity.Z).Magnitude
+	
+	local RayParams = RaycastParams.new()
+	RayParams.FilterDescendantsInstances = {player.Character}
+	RayParams.FilterType = Enum.RaycastFilterType.Exclude
+	
+	local HeightRay = Vector3.new(0,-100,0)
+	local HeightRayResult = workspace:Raycast(hrp.Position, HeightRay, RayParams)
+	local DistanceToGround = HeightRayResult and (hrp.Position - HeightRayResult.Position).Magnitude or 100
+	
+	local gravity = workspace.Gravity
+	local maxAllowedY = -(math.sqrt(2 * gravity * DistanceToGround))
+	
+	local RayDirection = Vector3.new(0,-5.5,0)
+	local RayResult = workspace:Raycast(hrp.Position, RayDirection, RayParams)
+	local IsInAir = not RayResult
 	
 	local suspicious = false
 	
-	if horizontal > config.HorizontalSpeedTreshold then
+	if vertical < maxAllowedY and IsInAir then
 		suspicious = true
 	end
 	
@@ -15,19 +33,27 @@ function Antifly.Check(player, hrp, data, config)
 		suspicious = true
 	end
 	
+	if horizontal > config.HorizontalSpeedTreshold then
+		suspicious = true
+	end
+	
 	if suspicious then
 		data.Flags += 1
 	else
 		data.Flags = math.max(0, data.Flags - config.FlagsDecay)
-		data.LastValidPosition = hrp.Position
+		if not IsInAir then
+			data.LastValidPosition = hrp.Position
+		end
 	end
-	
-	print(player.Name, "Suspicious:", suspicious)
+	-- UnComment this to debug lol
+	--print(player.Name, "Suspicious:", suspicious)
+	--print(HeightRayResult)
 	
 	if data.Flags >= config.FlagsLimit then
 		hrp.CFrame = CFrame.new(data.LastValidPosition)
 		hrp.AssemblyLinearVelocity = Vector3.zero
 		data.Flags = 0
+		
 	end
 end
 
